9d900a8347f7030fc99069cc6d1970966d7d4d83
Michał Beń <be.micha.mb@gmail.com>
2023-04-15 22:59:01 +0200

    ab: Move ordering
    Dodanie kolejki priorytetowej, wykorzystanie MVA-LVV dla bić i killer moves.

--- alfa/alfa.py
+++ alfa/alfa.py
@@ -4,55 +4,80 @@ import chess
 import eval
 import board_interface
 import zorba
+#import time
+from queue import PriorityQueue
 
 infinity = int(1e6)
 CHECKMATE = infinity - 1
 DRAW = 0
 HASHES = {}
+Max_Depth = 125
+killer_list = []
+for i in range(Max_Depth):
+    killer_list.append([])
+Branches_Checkd = 0  # Aby sprawdzić czy optymalizacje ucina głałęzie zliczamy uciętegałęzie
+ply_counter = 0
 
+
+def PriorityList(pos, hash,depth): # TO DO: W tym miejscu kolejka priorytetowa
+    global killer_list
+    global ply_counter_counter
+    moves = list(board_interface.get_moves(pos))
+    move = ""
+    if hash in HASHES:  # Najpierw sprawdzamy wierzchołek, który wcześniej uznaliśmy za najlepszy
+        res = HASHES[hash]
+        move = res[3]
+    moves = board_interface.filter(pos, moves, move, killer_list) #only_captures - Przeszukanie w miejscu
+    # killer moves
+    #for move in killer_list[ply_counter-depth]:
+     #  moves.remove(move)
+      # moves.insert(0, move)
+        
+    # 1 HashMove
+    # 2 KillerMoves
+    # 3 Captures
+    # 4 Rest
+    return moves
 
 # Wstępna wersja alpha bety.
 def AlphaBeta(pos, depth, alpha, beta, hash, StartDepth):
     global BestMove  # Przechowujemy najlepszy znaleziony do tej pory ruch (na poziomie)
+    global killer_list
+    global Branches_Checkd
+    global ply_counter
     if hash in HASHES:  # odwiedziliśmy już tą pozycję wcześniej
         res = HASHES[hash]
         if res[0] >= depth:  # Interesują nas tylko wyniki znalezione na conajmniej takiej samej głębokości
             v = res[1]
             if res[2] == "CUT":
                 if beta <= v:  # Jeżeli best so far(BSF) wierzchołka typu cut jest mniejszy od naszej bety, to nie musimy szukac dalej
+                    Branches_Checkd += 1
                     return v
                 if alpha < v:  # Jeżeli jest większy od naszej alphy to analogicznie możemy tą alpha nadpisać, ale pozycję i tak trzeba przeszukać
                     alpha = v
             if res[2] == "ALL":  # Analogicznie dla wierzchołka typu "ALL"
                 if v <= alpha:
+                    Branches_Checkd += 1
                     return v
                 if v < beta:
                     beta = v
             if res[2] == "PV":  # Jeżeli wierzchołek jest typu "PV" to z zadaną dokładnością wyznazczyliśmy już BSF i możemy zwrócić
+                Branches_Checkd += 1
                 return v
 
     if depth <= 0:
         # Poniższą linijkę należy odkomentować a jeszcze poniższą wciąć, żeby
         # włączyć pogłębianie do końca wymian.
-        # if not board_interface.has_captures(board):
+        #if not board_interface.has_captures(pos): #Wszystkie bicia czy tylko na polu którym było wcześniej
         return eval.eval(pos)
-
+        #moves = board_interface.filter_only_captures(pos, list(board_interface.get_moves(pos)))
+    else:
+        moves = PriorityList(pos,hash, depth)
     BestTempMove = ""
     BestSoFar = -infinity
-    moves = list(board_interface.get_moves(pos))  # TO DO: W tym miejscu kolejka priorytetowa
-
-    if depth <= 0:
-        # print(depth)
-        moves = board_interface.filter_only_captures(board, moves)
-    if hash in HASHES:  # Najpierw sprawdzamy wierdzchołek, który wcześniej uznaliśmy za najlepszy
-        res = HASHES[hash]
-        move = res[3]
-        if move in moves:
-            moves.remove(move)
-            moves.insert(0, move)
-
-    for move in moves:
+    while not moves.empty():
         # Wyznaczamy hash dla danej pozycji i dla danego ruchu
+        move = moves.get()[2]
         newHash = zorba.hash(pos, hash, move, pos.turn)
         pos = board_interface.make_move(pos, move)
         # Rekurencyjnie wchodzimy glebiej w pozycje, zmiana gracza ktory wykonuje ruch
@@ -66,9 +91,13 @@ def AlphaBeta(pos, depth, alpha, beta, hash, StartDepth):
             BestSoFar = val
             BestTempMove = move
         if BestSoFar >= beta:
+            if not board_interface.is_capture(pos, move) and move not in killer_list:
+                killer_list[ply_counter-depth].insert(0, move)
+                killer_list[ply_counter-depth] = killer_list[ply_counter-depth][1:]
             # Znaleziony ruch juz jest za dobry dla przeciwnika
             # Nie interesuje nas czy przeciwnik zagra potencjalnie jeszcze lepszy ruch, przerywamy - Typ CUT
             HASHES[hash] = (depth, BestSoFar, "CUT", BestTempMove)
+            Branches_Checkd += 1
             return BestSoFar
         alpha = max(alpha, BestSoFar)
     # Przeszukaliśmy wszystkie mozliwe ruchy
@@ -79,14 +108,17 @@ def AlphaBeta(pos, depth, alpha, beta, hash, StartDepth):
         HASHES[hash] = (depth, BestSoFar,  "PV", BestTempMove)
     return BestSoFar
 
-
 def Search(board, depth):
+    global ply_counter
     # Iterative Deepening, domyslnie to powinno byc wolane przez main
     HASHES = {}
     posHash = zorba.hashInit(board)
+    #ts=time.time()
     for i in range(1, depth + 1):
+        ply_counter = i
         print(AlphaBeta(board, i, -infinity, infinity, posHash, i), BestMove)
-
+        #print(time.time()-ts)
 
 board = chess.Board()
-Search(board, 10)
+Search(board, 5)
+#print(Branches_Checkd)
--- alfa/board_interface.py
+++ alfa/board_interface.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import chess
-
+from queue import PriorityQueue
 # Moduł dla przeszukiwania drzewa gry do rozmawiania z biblioteką, na razie python-chess
 # Jak już będziemy mieli tę rustową, to będziemy tu z nią gadać i te funkcje wywoływać w przeszukiwaniu
 
@@ -71,10 +71,48 @@ def has_captures(board):
             return True
     return False
 
-
 def filter_only_captures(board, moves):
     res = []
     for m in moves:
         if is_capture(board, m):
             res.append(m)
     return res
+
+def filter_only_not_captures(board, moves):
+    res = []
+    for m in moves:
+        if not is_capture(board, m):
+            res.append(m)
+    return res
+
+# MVV_LVA[victim][attacker]
+MVV_LVA= [
+    [0, 0, 0, 0, 0, 0, 0],       # victim K, attacker K, Q, R, B, N, P, None
+    [50, 51, 52, 53, 54, 55, 0], # victim Q, attacker K, Q, R, B, N, P, None
+    [40, 41, 42, 43, 44, 45, 0], # victim R, attacker K, Q, R, B, N, P, None
+    [30, 31, 32, 33, 34, 35, 0], # victim B, attacker K, Q, R, B, N, P, None
+    [20, 21, 22, 23, 24, 25, 0], # victim N, attacker K, Q, R, B, N, P, None
+    [10, 11, 12, 13, 14, 15, 0], # victim P, attacker K, Q, R, B, N, P, None
+    [0, 0, 0, 0, 0, 0, 0],       # victim None, attacker K, Q, R, B, N, P, None
+]
+#Piece_Position= ["K","Q","R","B","N","P",None] # Pozycja w tabeli MVV_LVA
+
+def filter(board, moves, hashmove, killerlist):
+    counter=0
+    Q = PriorityQueue()
+    for move in moves:
+        if move == hashmove:
+            Q.put((-12345, counter, move))
+        elif move in killerlist:
+            Q.put((-12344, counter, move))
+        elif is_capture(board, move):
+            victim = 6 - piece_at(board,to_square(move)) # Znajdujemy typ bitej bierki
+            attacker = 6 - piece_at(board,from_square(move))
+            Q.put((-MVV_LVA[victim][attacker], counter,move))
+        else:
+            Q.put((0,counter,move))
+        counter += 1
+    return Q
+
+
+
