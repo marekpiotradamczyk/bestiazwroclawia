b154aae7618db84aa559740ec38ccbe125edf681
Mateusz Burdyna <316662@uwr.edu.pl>
2023-12-01 16:51:43 +0100

    added deploy to gitignore

--- rust_mbu/rust_engine/deploy-train.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-cross build --release --target x86_64-unknown-linux-gnu
-echo "[+] Build complete"
-
-rsync --update target/x86_64-unknown-linux-gnu/release/engine install@156.17.4.12:/home/bestiapodgrunwaldem/arena/engines/morphe_engine
-echo "[+] Copy complete"
-echo "[+] Done"
