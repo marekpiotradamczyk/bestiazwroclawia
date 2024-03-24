3be78ff2ca590c317477562e589c29b3eb3f8574
mateusz2173 <mateusz_burdyna@protonmail.com>
2024-03-24 19:21:02 +0100

    Fixed pinned bonus

--- rust_mbu/rust_engine/engine/src/engine/eval/pin_bonus.rs
+++ rust_mbu/rust_engine/engine/src/engine/eval/pin_bonus.rs
@@ -9,5 +9,5 @@ pub fn bonus_for_absolute_pins(pos: &Position, move_gen: Arc<MoveGen>) -> i32 {
     let white_pinned_count = move_gen.pinned_pieces(pos, Color::White).count() as i32;
     let black_pinned_count = move_gen.pinned_pieces(pos, Color::Black).count() as i32;
 
-    (white_pinned_count - black_pinned_count) * PINNED_PIECE_BONUS
+    (black_pinned_count - white_pinned_count) * PINNED_PIECE_BONUS
 }
