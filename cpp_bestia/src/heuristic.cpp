5cb439df1de0af88bef3f22c26befa8ea7ecd5a2
Grzegorz Zielonka <zielonkagrzegorz3@gmail.com>
2024-05-14 00:43:38 +0200

    Add basic heuristic (material counting)

---
+++ cpp_bestia/src/heuristic.cpp
@@ -0,0 +1,23 @@
+#include "heuristic.hpp"
+
+I16 getPieceCount(const chess::Board &board, chess::PieceType pieceType,
+                  chess::Color color) {
+  return std::popcount(board.pieces(pieceType, color).getBits());
+}
+
+I16 heuristic(const chess::Board &board) {
+  using namespace chess;
+  PieceType type[] = {PieceType::PAWN, PieceType::KNIGHT, PieceType::BISHOP,
+                      PieceType::ROOK, PieceType::QUEEN};
+  I16 value[] = {100, 300, 325, 500, 900};
+  Color color[] = {Color::WHITE, Color::BLACK};
+  I16 colorMultiplier[] = {1, -1};
+  I16 returnValue = 0;
+  for (int i = 0; i < 2; i++) {   // for every color
+    for (int j = 0; j < 5; j++) { // for every piece type
+      returnValue += colorMultiplier[i] * value[j] *
+                     getPieceCount(board, type[j], color[i]);
+    }
+  }
+  return returnValue;
+}
