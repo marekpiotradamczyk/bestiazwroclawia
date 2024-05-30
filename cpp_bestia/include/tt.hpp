ef3cdcd4ec7b3e4e5f7c3d3bdce7e3b1c471bee2
Dominik Kowalczyk <xkowal99@gmail.com>
2024-05-30 14:44:33 +0200

    Add Lock-less TT implementation.
    Implementation based on https://www.chessprogramming.org/Shared_Hash_Table\#Lockless.
    TTData stores score, move, depth, age and type.

--- cpp_bestia/include/tt.hpp
+++ cpp_bestia/include/tt.hpp
@@ -2,35 +2,78 @@
 
 #include <stdint.h>
 
+#include <atomic>
+#include <optional>
 #include <vector>
 
 #include "chess.hpp"
 
 namespace chess {
 
-struct TTEntry {
+// Implementation based on
+// https://www.chessprogramming.org/Shared_Hash_Table#Lockless
+
+class TTData {
+ public:
   enum class Type : uint8_t {
     NONE,
-    UPPER,
-    LOWER,
+    ALPHA,
+    BETA,
     EXACT,
   };
 
-  int32_t score;
-  Move move;
-  uint8_t depth;
-  Type type = Type::NONE;
+  TTData(uint64_t data);
+
+  int32_t score() const;
+  Move move() const;
+  uint8_t depth() const;
+  uint8_t age() const;
+  Type type() const;
+
+  static uint64_t pack(int32_t score, Move move, uint8_t depth, uint8_t age,
+                       Type type);
+
+ private:
+  static constexpr uint64_t SCORE_BITS = 32;
+  static constexpr uint64_t MOVE_BITS = 16;
+  static constexpr uint64_t DEPTH_BITS = 7;
+  static constexpr uint64_t AGE_BITS = 7;
+  static constexpr uint64_t TYPE_BITS = 2;
+
+  static_assert(SCORE_BITS + MOVE_BITS + DEPTH_BITS + AGE_BITS + TYPE_BITS <=
+                64);
+
+  static constexpr uint64_t SCORE_SHIFT = 0;
+  static constexpr uint64_t SCORE_MASK = (1ULL << SCORE_BITS) - 1;
+  static constexpr uint64_t MOVE_SHIFT = SCORE_SHIFT + SCORE_BITS;
+  static constexpr uint64_t MOVE_MASK = (1ULL << MOVE_BITS) - 1;
+  static constexpr uint64_t DEPTH_SHIFT = MOVE_SHIFT + MOVE_BITS;
+  static constexpr uint64_t DEPTH_MASK = (1ULL << DEPTH_BITS) - 1;
+  static constexpr uint64_t AGE_SHIFT = DEPTH_SHIFT + DEPTH_BITS;
+  static constexpr uint64_t AGE_MASK = (1ULL << AGE_BITS) - 1;
+  static constexpr uint64_t TYPE_SHIFT = AGE_SHIFT + AGE_BITS;
+  static constexpr uint64_t TYPE_MASK = (1ULL << TYPE_BITS) - 1;
+
+  uint64_t data_ = 0;
 };
 
 class TranspositionTable {
  public:
   TranspositionTable(uint64_t size_in_mb);
 
-  const TTEntry* operator[](uint64_t hash) const;
-  void add(uint64_t hash, int32_t score, Move move, uint8_t depth,
-           TTEntry::Type type);
+  std::optional<TTData> operator[](uint64_t hash) const;
+
+  void add(uint64_t hash, int32_t score, Move move, uint8_t depth, uint8_t age,
+           TTData::Type type);
 
  private:
+  struct TTEntry {
+    std::atomic<uint64_t> hash = 0;
+    std::atomic<uint64_t> data = 0;
+
+    void write(uint64_t hash, uint64_t data);
+  };
+
   std::vector<TTEntry> entries_;
 };
 
--- cpp_bestia/src/tt.cpp
+++ cpp_bestia/src/tt.cpp
@@ -2,24 +2,66 @@
 
 namespace chess {
 
+TTData::TTData(uint64_t data) : data_(data) {}
+
+int32_t TTData::score() const { return data_ & SCORE_MASK; }
+
+Move TTData::move() const { return (data_ >> MOVE_SHIFT) & MOVE_MASK; }
+
+uint8_t TTData::depth() const { return (data_ >> DEPTH_SHIFT) & DEPTH_MASK; }
+
+uint8_t TTData::age() const { return (data_ >> DEPTH_SHIFT) & DEPTH_MASK; }
+
+TTData::Type TTData::type() const {
+  return static_cast<Type>((data_ >> DEPTH_SHIFT) & DEPTH_MASK);
+}
+
+uint64_t TTData::pack(int32_t score, Move move, uint8_t depth, uint8_t age,
+                      Type type) {
+  return (static_cast<uint64_t>(score) << SCORE_SHIFT) |
+         (static_cast<uint64_t>(move.move()) << MOVE_SHIFT) |
+         (static_cast<uint64_t>(depth) << DEPTH_SHIFT) |
+         (static_cast<uint64_t>(age) << AGE_SHIFT) |
+         (static_cast<uint64_t>(type) << TYPE_SHIFT);
+}
+
 TranspositionTable::TranspositionTable(uint64_t size_in_mb) {
   entries_.resize(1024 * 1024 * size_in_mb / sizeof(TTEntry));
 }
 
-const TTEntry* TranspositionTable::operator[](uint64_t hash) const {
-  const auto index = hash % entries_.size();
-  return (entries_[index].type != TTEntry::Type::NONE ? &entries_[index]
-                                                      : nullptr);
+std::optional<TTData> TranspositionTable::operator[](uint64_t hash) const {
+  const uint64_t index = hash % entries_.size();
+
+  auto& entry = entries_[index];
+  const uint64_t entry_hash = entry.hash.load(std::memory_order_relaxed);
+  const uint64_t entry_data = entry.data.load(std::memory_order_relaxed);
+
+  return ((entry_hash ^ entry_data) == hash ? std::optional{TTData(entry_data)}
+                                            : std::nullopt);
 }
 
 void TranspositionTable::add(uint64_t hash, int32_t score, Move move,
-                             uint8_t depth, TTEntry::Type type) {
-  const auto index = hash % entries_.size();
-  auto& old_entry = entries_[index];
+                             uint8_t depth, uint8_t age, TTData::Type type) {
+  const uint64_t index = hash % entries_.size();
 
-  if (old_entry.type == TTEntry::Type::NONE || old_entry.depth < depth) {
-    old_entry = {score, move, depth, type};
+  auto& old_entry = entries_[index];
+  const uint64_t old_hash = old_entry.hash.load(std::memory_order_relaxed);
+
+  if (old_hash == 0) {
+    old_entry.write(hash, TTData::pack(score, move, depth, age, type));
+    return;
   }
+
+  auto old_data = TTData(old_entry.data.load(std::memory_order_relaxed));
+  if (old_data.age() < age ||
+      (old_data.age() == age && old_data.depth() < depth)) {
+    old_entry.write(hash, TTData::pack(score, move, depth, age, type));
+  }
+}
+
+void TranspositionTable::TTEntry::write(uint64_t hash, uint64_t data) {
+  this->hash.store(hash ^ data, std::memory_order_relaxed);
+  this->data.store(data, std::memory_order_relaxed);
 }
 
 }  // namespace chess
