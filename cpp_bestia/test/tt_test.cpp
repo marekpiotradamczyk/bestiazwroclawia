07c4a749f649e3df4596345b7b18c1e76fa92619
DomiKoPL <xkowal99@gmail.com>
2024-06-17 15:01:03 +0200

    Add multithreading test.

--- cpp_bestia/test/tt_test.cpp
+++ cpp_bestia/test/tt_test.cpp
@@ -2,6 +2,9 @@
 
 #include <gmock/gmock.h>
 
+#include <thread>
+#include <vector>
+
 using chess::TranspositionTable;
 using chess::TTData;
 using testing::Optional;
@@ -15,4 +18,26 @@ TEST(TT, AddsAndGetsTheSameData) {
   tt.add(hash, data);
 
   EXPECT_THAT(tt[hash], Optional(data));
+}
+
+TEST(TT, ManyThreadsAddsTheSameValue_EntryIsInvalid) {
+  auto tt = TranspositionTable(1);
+  uint64_t hash = 1;
+  const int NUM_THREADS = 32;
+
+  // Creates threads that will write different data to TT under the same hash.
+  std::vector<std::thread> threads;
+  for (int i = 0; i < NUM_THREADS; ++i) {
+    threads.emplace_back([&]() {
+      tt.add(hash, {/*score=*/i, /*move=*/{}, /*depth=*/2, /*age=*/10,
+                    TTData::Type::EXACT});
+    });
+  }
+  for (auto& thread : threads) {
+    thread.join();
+  }
+
+  // Actually this test is not 100% guaranteed to pass.
+  // I'm not sure what are the chances for it to fail.
+  EXPECT_FALSE(tt[hash].has_value());
 }
